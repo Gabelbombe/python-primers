3caab76a4ad2d971a3a91812c15f379404f36eed
ehime <dodomeki@gmail.com>
2017-05-13 20:26:49 -0700

    Adding float points

--- Basics/compare.py
+++
@@ -1,9 +0,0 @@
-# Arithmetic operations
-
-# Variable setting
-print 'x = 4 '  ; x = 4
-print 'y = 7 '  ; y = 7
-print 'x * y '  , x * y   # Variable setting
-print '2 + 2  ' , 2+2     # Addition
-print '4 / 3  ' , 4/3     # Division    (no float)
-print '3 - 8 '  , 3-8     # Subtraction (negatives)
