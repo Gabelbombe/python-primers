9a8b8ba8c237e276c7f9f5ba1b3a4ef56bcea631
ehime <dodomeki@gmail.com>
2017-05-13 22:39:03 -0700

    Octal issue solved, notes in script

--- Basics/002-otherbases.py
+++ Basics/002-otherbases.py
@@ -6,6 +6,13 @@ print 'bin(x) '   , bin(x)                  # '0b10011010010'
 print 'oct(x) '   , oct(x)                  # '02322' -> tracking octal misrepresentation at: https://goo.gl/Hab1e1
 print 'hex(x) '   , hex(x)                  # '0x4d2'
 
+# NOTE: baked in oct() should be superceded with `octal = '{:#o}'.format` if 0oXXX denomination is to be required
+# NOTE: both 0oXXX and 0XXX WILL EVALUATE CORRECTLY
+
+# Example:
+# ⌘ python -c "print \"%d\" % 0o2322" '1234'
+# ⌘ python -c "print \"%d\" % 02322"  '1234'
+
 # Using the format() function to suppress prefixes
 print 'format(x, \'b\')' , format(x, 'b')   # bin conversion
 print 'format(x, \'o\')' , format(x, 'o')   # oct conversion
