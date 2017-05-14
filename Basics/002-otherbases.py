a53cee2a5c601da659f10b5a2a1dc0f8de01089e
ehime <dodomeki@gmail.com>
2017-05-13 22:22:39 -0700

    tracking misrepresented octal prefix at: https://goo.gl/Hab1e1

--- Basics/002-otherbases.py
+++ Basics/002-otherbases.py
@@ -3,7 +3,7 @@
 # Base conversion operations
 print 'x = 1234 ' ; x = 1234                # all numbers are base10 derivs
 print 'bin(x) '   , bin(x)                  # '0b10011010010'
-print 'oct(x) '   , oct(x)                  # '02322' -> missing prefix??? expected: 0o02322???
+print 'oct(x) '   , oct(x)                  # '02322' -> tracking octal misrepresentation at: https://goo.gl/Hab1e1
 print 'hex(x) '   , hex(x)                  # '0x4d2'
 
 # Using the format() function to suppress prefixes
