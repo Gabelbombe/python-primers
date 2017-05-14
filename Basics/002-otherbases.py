e88d2a4d5120240b0d191269eea373112c0bcbfb
ehime <dodomeki@gmail.com>
2017-05-13 21:56:37 -0700

    base ops, from base10 to bin -> oct -> hex

---
+++ Basics/002-otherbases.py
@@ -0,0 +1,7 @@
+# Other Base operations
+
+# Base conversion operations
+print 'x = 1234 ' ; x = 1234    # base10
+print 'bin(x) '   , bin(x)      # '0b10011010010'
+print 'oct(x) '   , oct(x)      # '02322'
+print 'hex(x) '   , hex(x)      # '0x4d2'
