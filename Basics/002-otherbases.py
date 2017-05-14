9efdfcd6357547c2e57bb08db830522b66347095
ehime <dodomeki@gmail.com>
2017-05-13 22:11:43 -0700

    Bad oct() function not posting prefix

--- Basics/002-otherbases.py
+++ Basics/002-otherbases.py
@@ -1,7 +1,12 @@
 # Other Base operations
 
 # Base conversion operations
-print 'x = 1234 ' ; x = 1234    # base10
-print 'bin(x) '   , bin(x)      # '0b10011010010'
-print 'oct(x) '   , oct(x)      # '02322'
-print 'hex(x) '   , hex(x)      # '0x4d2'
+print 'x = 1234 ' ; x = 1234                # all numbers are base10 derivs
+print 'bin(x) '   , bin(x)                  # '0b10011010010'
+print 'oct(x) '   , oct(x)                  # '02322' -> missing prefix??? expected: 0o02322???
+print 'hex(x) '   , hex(x)                  # '0x4d2'
+
+# Using the format() function to suppress prefixes
+print 'format(x, \'b\')' , format(x, 'b')   # bin conversion
+print 'format(x, \'o\')' , format(x, 'o')   # oct conversion
+print 'format(x, \'x\')' , format(x, 'x')   # hex conversion
